fd7988fb4934768ed7abc9455f88e315557fffff
Karim <karem109k@gmail.com>
2023-12-22 09:53:34 +0200

    Product [ Get && Edit && Delete ]

--- controllers/Product.controller.js
+++ controllers/Product.controller.js
@@ -2,6 +2,7 @@ const Product = require("../models/Product");
 const Category = require("../models/Category");
 const SubCategory = require("../models/SubCategory");
 const Brand = require("../models/Brand");
+const Review = require("../models/Review");
 
 const { isMongoId } = require("validator");
 const errorModel = require("../utils/errorModel");
@@ -46,23 +47,81 @@ exports.createProduct = async (req, res, next) => {
 
         const product = await Product.create({ seller: _id, title, description, category: categoryData.name, price, media, ...data })
 
-        res.status(200).json(product)
+        res.status(201).json(product)
     } catch (error) { next(error) }
 }
 
 // Get Product
 exports.getProduct = async (req, res, next) => {
-    res.send("Get Product");
+    const { productId } = req.params;
+    if (!isMongoId(productId)) return next(errorModel(400, "productId Id Is Invalid"));
+
+    try {
+        const product = await Product.findById(productId, ["-favorited", "-sold"])
+            .populate("seller", ["name", "image"]);
+        if (!product) return next(errorModel(400, "Product not found"));
+
+        res.status(200).json(product);
+    } catch (error) { next(error) }
 }
 
 // Edit Product
 exports.editProduct = async (req, res, next) => {
-    res.send("Edit Product");
+    const { productId } = req.params;
+    const { title, description, category, subCategory, price, brand, stock, discount } = req.body;
+    const files = req.files;
+
+    if (files.length === 0 && Object.keys(req.body).length === 0) return next(errorModel(400, "Provide at least one field"));
+    if (!isMongoId(productId)) return next(errorModel(400, "productId Id Is Invalid"));
+
+    try {
+        const product = await Product.findById(productId, ["-favorited", "-sold"])
+            .populate("seller", ["name", "image"]);
+        if (!product) return next(errorModel(400, "Product not found"));
+
+        if (category) {
+            const categoryData = await Category.findById(category);
+            if (!categoryData) return next(errorModel(400, "Category not Found"));
+            product.category = categoryData;
+        }
+        if (subCategory) {
+            if (!isMongoId(subCategory)) return next(errorModel(400, "SubCategory Id Is Invalid"));
+            const subCat = await SubCategory.findById(subCategory);
+            if (!subCat) return next(errorModel(400, "SubCategory not Found"));
+            product.subCategory = subCat.name;
+        }
+        if (brand) {
+            if (!isMongoId(brand)) return next(errorModel(400, "Brand Id Is Invalid"));
+            const brandData = await Brand.findById(brand);
+            if (!brandData) return next(errorModel(400, "Brand not Found"));
+            product.brand = brandData.name;
+        }
+        if (title) product.title = title;
+        if (description) product.description = description;
+        if (price) product.price = price;
+        if (stock) product.stock = stock;
+        if (discount) product.discount = discount;
+        const updatedProduct = await product.save();
+
+        res.status(200).json(updatedProduct._doc);
+    } catch (error) { next(error) }
 }
 
 // Delete Product
 exports.deleteProduct = async (req, res, next) => {
-    res.send("Delete Product");
+    const { productId } = req.params;
+    if (!isMongoId(productId)) return next(errorModel(400, "productId Id Is Invalid"));
+
+    try {
+        const product = await Product.findById(productId);
+        if (!product) return next(errorModel(400, "Product not found"));
+
+        // Delete product images ...
+
+        await Review.deleteMany({ productId });
+        await product.deleteOne();
+        res.status(200).json({ msg: "Product deleted successfully" });
+    } catch (error) { next(error) }
 }
 
 // Review Product
