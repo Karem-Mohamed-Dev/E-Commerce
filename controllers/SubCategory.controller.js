c060dc9cf9e02c257d7fc7d04b16bec525814882
Karim <karem109k@gmail.com>
2023-12-22 08:42:56 +0200

    Finishing SubCategory Controllers

--- controllers/SubCategory.controller.js
+++ controllers/SubCategory.controller.js
@@ -1,25 +1,14 @@
 const SubCategory = require('../models/SubCategory');
 const Category = require('../models/Category');
+const Product = require('../models/Product');
 
 // Get SubCategorys
 exports.getSubCategorys = async (req, res, next) => {
-    const page = +req.params.page || 1;
-    const limit = 10;
-    const skip = (page - 1) * limit;
+    const { categoryId } = req.params;
 
     try {
-        const subCategoriesCount = await SubCategory.countDocuments({});
-        const subCategories = await SubCategory.find({})
-            .skip(skip).limit(limit);
-
-        res.status(200).json({
-            result: subCategoriesCount,
-            paginationData: {
-                currentPage: page,
-                totalPages: Math.ceil(subCategoriesCount / limit)
-            },
-            subCategories
-        })
+        const subCategories = await SubCategory.find({ categoryId }, ['-slug', '-categoryId']);
+        res.status(200).json(subCategories);
     } catch (error) { next(error) }
 }
 
@@ -32,25 +21,55 @@ exports.createSubCategory = async (req, res, next) => {
     if (!isMongoId(categoryId)) return next(errorModel(400, "Please Provide a Valid Category Id"));
 
     const slug = name.split(" ").join("-");
-
     try {
-        const category = await Category.findById(categoryId);
+        const category = await Category.findById(categoryId, ["categoryId", "slug"]);
         if (!category) return next(errorModel(400, "No Category Found"));
 
         // Image Upload
         const image = { url: "", publicId: "" }; // Simulation until add image upload
 
         const subCategory = await SubCategory.create({ name, slug, image, categoryId })
-        res.status(200).json({ subCategory: subCategory._doc });
+        res.status(200).json(subCategory);
     } catch (error) { next(error) }
 }
 
 // Edit SubCategory
 exports.editSubCategory = async (req, res, next) => {
-    res.send('Edit SubCategory');
+    const { subId } = req.params;
+    const { name } = req.body;
+    const file = req.file;
+    if (!file && !name) return next(errorModel(400, "Please Provide at least one field"))
+    if (!isMongoId(subId)) return next(errorModel(400, "Please Provide a Valid Category Id"));
+
+    try {
+        const subCategory = await SubCategory.findById(subId);
+        if (!subCategory) return next(errorModel(404, "SubCategory not found"));
+
+        if (file) {
+            // delete old image
+            // upload new image
+        }
+        if (name) {
+            subCategory.name = name;
+            subCategory.slug = name.split(" ").join("-");
+        }
+        await subCategory.save();
+
+        res.status(200).json(subCategory);
+    } catch (error) { next(error) }
 }
 
 // Delete SubCategory
 exports.deleteSubCategory = async (req, res, next) => {
-    res.send('Delete SubCategory');
+    const { subId } = req.params;
+    if (!isMongoId(subId)) return next(errorModel(400, "Please Provide a Valid Category Id"));
+
+    try {
+        const subCategory = await SubCategory.findById(subId, 'name');
+        if (!subCategory) return next(errorModel(404, "SubCategory not found"));
+        await SubCategory.deleteOne();
+        await Product.updateMany({ subCategory: subCategory.name }, { subCategory: null });
+
+        res.status(200).json({msg: "SubCategory deleted successfully"});
+    } catch (error) { next(error) }
 }
